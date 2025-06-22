8988bb82234b4f358725430fc940e877fa0d985c
Jibin George <jibingeorge1964@gmail.com>
2025-06-22 17:41:22 +0530

    Refactoring

--- Home.py
+++ Home.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="CyberSleuth",
+    page_title="TraceSleuth",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
-st.title("Welcome to CyberSleuth Toolkit 🕵️")
+st.title("Welcome to TraceSleuth Toolkit 🕵️")
 
 st.write("Your all-in-one cybersecurity analysis and response platform.")
 st.info("Select a tool from the sidebar to get started.")
